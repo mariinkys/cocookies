787ec61c8b6c09324b9f446ee281ce58fabfc564
mariinkys <kysgeneral.nrgua@slmail.me>
2024-12-26 14:37:03 +0100

    wip: rethink how I'm going to do things

--- src/pages/view_edit_recipe.rs
+++ src/pages/view_edit_recipe.rs
@@ -9,37 +9,14 @@ use leptos_router::params::Params;
 
 #[derive(Params, PartialEq)]
 struct RecipeParams {
-    id: Option<usize>,
+    id: Option<i32>,
 }
 
 #[component]
 pub fn ViewEditRecipe() -> impl IntoView {
     let set_toast: WriteSignal<ToastMessage> = expect_context();
     let params = use_params::<RecipeParams>();
-
-    let id = move || {
-        params
-            .read()
-            .as_ref()
-            .ok()
-            .and_then(|params| params.id)
-            .unwrap_or_default()
-    };
-    if id() == 0 {
-        set_toast.set(ToastMessage {
-            message: String::from("Missing or Invalid Id Params"),
-            toast_type: ToastType::Error,
-            visible: true,
-        });
-        // TODO: Can't redirect with navigate cuz crashes (thread 'actix-rt|system:0|arbiter:8' panicked)
-        // It works inside a spawn_local but still gives the error so better not to?
-        // spawn_local(async {
-        //  navigate("/", Default::default());
-        //});
-    } else {
-        // Pass the recipe id to all children components
-        provide_context(id());
-    }
+    let id = params.read().as_ref().ok().and_then(|params| params.id);
 
     view! {
         <Suspense fallback=move || view! { <PageLoadingComponent/> }>
