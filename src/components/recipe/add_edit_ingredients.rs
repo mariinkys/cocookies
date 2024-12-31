2cc24b0ef68dad1b550773912d81d033d45af054
mariinkys <kysgeneral.nrgua@slmail.me>
2024-12-31 10:58:11 +0100

    fix: hydration issues on page reload

--- src/components/recipe/add_edit_ingredients.rs
+++ src/components/recipe/add_edit_ingredients.rs
@@ -4,7 +4,10 @@ use crate::{
     api::recipe_ingredients::{
         get_all_recipe_ingredients, AddRecipeIngredients, UpdateRecipeIngredients,
     },
-    components::toast::{ToastMessage, ToastType},
+    components::{
+        page_loading::PageLoadingComponent,
+        toast::{ToastMessage, ToastType},
+    },
 };
 
 #[component]
@@ -44,107 +47,111 @@ pub fn ViewEditIngredientsComponent(recipe_id: i32) -> impl IntoView {
     view! {
         <div class="w-full card shadow-xl">
             <div class="card-body">
-                { move || {
-                    // TODO: When you refresh the page something inside here causes hydration errors
-                    recipe_ingredients_resource.get().map(move |x| {
-                        x.map(move |recipe_ingredient_result| {
-                            let recipe_ingredients = RwSignal::new(recipe_ingredient_result);
-                            view! {
-                                <For each=move || recipe_ingredients.get() key=|ingredient| ingredient.recipe_ingredient_id children=move |recipe_ingredient| {
-                                    let model = RwSignal::new(recipe_ingredient);
-
+                <Suspense fallback= move || view! { <PageLoadingComponent/> }>
+                    <ErrorBoundary fallback=|error| view! {
+                        <p class="text-3xl text-center text-red-500">"An error occurred: " {format!("{:?}", error)}</p>
+                    }>
+                        { move || {
+                            recipe_ingredients_resource.get().map(move |x| {
+                                x.map(move |recipe_ingredient_result| {
+                                    let recipe_ingredients = RwSignal::new(recipe_ingredient_result);
                                     view! {
-                                    <div class="w-full card shadow-xl">
-                                        <div class="card-body">
-                                            <ActionForm action=update_recipe_ingredient>
-                                                // TODO: Fix flex wrap on mobile
-                                                <div class="flex gap-2 items-center">
-                                                    // We need the id for the update but we don't want to show it.
-                                                    <input type="hidden" name="recipe_ingredient_id" autocomplete="off" prop:value={move || model.get().recipe_ingredient_id.unwrap_or_default()}/>
+                                        <For each=move || recipe_ingredients.get() key=|ingredient| ingredient.recipe_ingredient_id children=move |recipe_ingredient| {
+                                            let model = RwSignal::new(recipe_ingredient);
 
-                                                    // Recipe Ingredient: ingredient_name
-                                                    <div class="w-full">
-                                                        <div class="label p-0">
-                                                            <span class="label-text">"Ingredient Name"</span>
+                                            view! {
+                                            <div class="w-full card shadow-xl">
+                                                <div class="card-body">
+                                                    <ActionForm action=update_recipe_ingredient>
+                                                        <div class="flex flex-wrap gap-2 items-center">
+                                                            // We need the id for the update but we don't want to show it.
+                                                            <input type="hidden" name="recipe_ingredient_id" autocomplete="off" prop:value={move || model.get().recipe_ingredient_id.unwrap_or_default()}/>
+
+                                                            // Recipe Ingredient: ingredient_name
+                                                            <div class="flex-1 min-w-[200px]">
+                                                                <div class="label p-0">
+                                                                    <span class="label-text">"Ingredient Name"</span>
+                                                                </div>
+                                                                <input type="text"
+                                                                    class="input input-bordered w-full"
+                                                                    name="ingredient_name"
+                                                                    required
+                                                                    autocomplete="off"
+                                                                    prop:value={move || model.get().ingredient_name}
+                                                                    on:input=move |ev| {
+                                                                        model.update(|curr| {
+                                                                            curr.ingredient_name = event_target_value(&ev);
+                                                                        });
+                                                                    }
+                                                                />
+                                                            </div>
+
+                                                            // Recipe Ingredient: quantity
+                                                            // TODO: Can't input a decimal number on this input
+                                                            <div class="flex-1 min-w-[200px]">
+                                                                <div class="label p-0">
+                                                                    <span class="label-text">"Quantity"</span>
+                                                                </div>
+                                                                <input type="text"
+                                                                    class="input input-bordered w-full"
+                                                                    name="quantity"
+                                                                    required
+                                                                    autocomplete="off"
+                                                                    prop:value={move || model.get().quantity}
+                                                                    on:input=move |ev| {
+                                                                        if let Ok(value) = event_target_value(&ev).parse::<f64>() {
+                                                                            model.update(|curr| {
+                                                                                curr.quantity = value;
+                                                                            });
+                                                                        } else {
+                                                                            model.update(|curr| {
+                                                                                curr.quantity = 0.0;
+                                                                            });
+                                                                        }
+                                                                    }
+                                                                />
+                                                            </div>
+
+                                                            // Recipe: unit
+                                                            <div class="flex-1 min-w-[200px]">
+                                                                <div class="label p-0">
+                                                                    <span class="label-text">"Unit"</span>
+                                                                </div>
+                                                                <input type="text"
+                                                                    class="input input-bordered w-full"
+                                                                    name="unit"
+                                                                    autocomplete="off"
+                                                                    prop:value=move || model.get().unit.unwrap_or_default()
+                                                                    on:input=move |ev| {
+                                                                        if !event_target_value(&ev).is_empty() {
+                                                                            model.update(|curr| {
+                                                                                curr.unit = Some(event_target_value(&ev));
+                                                                            });
+                                                                        } else {
+                                                                            model.update(|curr| {
+                                                                                curr.unit = None;
+                                                                            });
+                                                                        }
+                                                                    }
+                                                                />
+                                                            </div>
+
+                                                            //TODO: Add Delete Button
+                                                            <button type="submit" class="btn btn-primary mt-[20px] sm:w-min w-full">"Update"</button>
                                                         </div>
-                                                        <input type="text"
-                                                            class="input input-bordered w-full"
-                                                            name="ingredient_name"
-                                                            required
-                                                            autocomplete="off"
-                                                            prop:value={move || model.get().ingredient_name}
-                                                            on:input=move |ev| {
-                                                                model.update(|curr| {
-                                                                    curr.ingredient_name = event_target_value(&ev);
-                                                                });
-                                                            }
-                                                        />
-                                                    </div>
-
-                                                    // Recipe Ingredient: quantity
-                                                    // TODO: Can't input a decimal number on this input
-                                                    <div class="w-full">
-                                                        <div class="label p-0">
-                                                            <span class="label-text">"Quantity"</span>
-                                                        </div>
-                                                        <input type="text"
-                                                            class="input input-bordered w-full"
-                                                            name="quantity"
-                                                            required
-                                                            autocomplete="off"
-                                                            prop:value={move || model.get().quantity}
-                                                            on:input=move |ev| {
-                                                                if let Ok(value) = event_target_value(&ev).parse::<f64>() {
-                                                                    model.update(|curr| {
-                                                                        curr.quantity = value;
-                                                                    });
-                                                                } else {
-                                                                    model.update(|curr| {
-                                                                        curr.quantity = 0.0;
-                                                                    });
-                                                                }
-                                                            }
-                                                        />
-                                                    </div>
-
-                                                    // Recipe: unit
-                                                    <div class="w-full">
-                                                        <div class="label p-0">
-                                                            <span class="label-text">"Unit"</span>
-                                                        </div>
-                                                        <input type="text"
-                                                            class="input input-bordered w-full"
-                                                            name="unit"
-                                                            autocomplete="off"
-                                                            prop:value=move || model.get().unit.unwrap_or_default()
-                                                            on:input=move |ev| {
-                                                                if !event_target_value(&ev).is_empty() {
-                                                                    model.update(|curr| {
-                                                                        curr.unit = Some(event_target_value(&ev));
-                                                                    });
-                                                                } else {
-                                                                    model.update(|curr| {
-                                                                        curr.unit = None;
-                                                                    });
-                                                                }
-                                                            }
-                                                        />
-                                                    </div>
-
-                                                    //TODO: Add Delete Button
-                                                    <button type="submit" class="btn btn-primary mt-[20px] w-min">"Update"</button>
+                                                    </ActionForm>
                                                 </div>
-                                            </ActionForm>
-                                        </div>
-                                    </div>
+                                            </div>
+                                            }
+                                        }/>
+
+
                                     }
-                                }/>
-
-
-                            }
-                        })
-                    })
-                }}
+                                })
+                            })
+                        }}
+                    </ErrorBoundary>
+                </Suspense>
                 <div class="w-full card shadow-xl">
                     <div class="card-body">
                         <ActionForm action=add_recipe_ingredient>
